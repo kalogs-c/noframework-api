72a7cb42d10a42763acbb1c56e2d71e67654cc63
Carlos Camilo <carloscamilocontato@gmail.com>
2021-10-12 10:36:43 -0300

    Managing errors

--- script.sh
+++ script.sh
@@ -3,3 +3,20 @@ curl localhost:3000/heroes
 
 echo "requesting flash"
 curl localhost:3000/heroes/1
+
+echo "requesting with wrong body"
+curl --silent -X POST \
+    --data-binary '{"invalid": "data"}' \
+    localhost:3000/heroes
+
+echo "creating chapolin"
+CREATE=$(curl --silent -X POST \
+    --data-binary '{"name": "Chapolin", "age": 101, "power": "strength"}' \
+    localhost:3000/heroes)
+echo $CREATE
+
+ID=$(echo $CREATE | jq .id)
+echo $ID
+
+echo "requesting chapolin"
+curl localhost:3000/heroes/$ID
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const PORT = 3000;
 
 const DEFAULT_HEADER = { "Content-Type": "application/json" };
 
+const HeroFactory = require("./factories/heroFactory");
+const heroService = HeroFactory.generateInstance();
+const Hero = require("./entities/hero");
+
 const routes = {
   default: (request, response) => {
     response.write("Hello :)");
@@ -10,9 +14,41 @@ const routes = {
   },
   "/heroes:get": async (request, response) => {
     const { id } = request.queryString;
-    console.log({ id });
+    const heroes = await heroService.find(id);
+    response.write(JSON.stringify({ results: heroes }));
     return response.end();
   },
+  "/heroes:post": async (request, response) => {
+    for await (const data of request) {
+      try {
+        const item = JSON.parse(data);
+        const hero = new Hero(item);
+        const { error, valid } = hero.isValid();
+        if (!valid) {
+          response.writeHead(400, DEFAULT_HEADER);
+          response.write(JSON.stringify({ error: error.join(",") }));
+          return response.end();
+        }
+
+        const id = await heroService.create(hero);
+        response.writeHead(200, DEFAULT_HEADER);
+        response.write(
+          JSON.stringify({ success: "User created with success!", id })
+        );
+      } catch (error) {
+        handleError(response)(error);
+      }
+    }
+  },
+};
+
+const handleError = (response) => {
+  return (error) => {
+    console.error("Ops, something bad happened", error);
+    response.writeHead(500, DEFAULT_HEADER);
+    response.write(JSON.stringify({ error: "Internal Server Error" }));
+    return response.end();
+  };
 };
 
 const handler = (request, response) => {
@@ -24,7 +60,7 @@ const handler = (request, response) => {
   response.writeHead(200, DEFAULT_HEADER);
 
   const chosen = routes[key] || routes.default;
-  return chosen(request, response);
+  return chosen(request, response).catch(handleError(response));
 };
 
 http
